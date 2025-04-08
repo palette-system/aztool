ba7c95ffc086b2975c6f13f9d11f8e2d1469268a
yo1sjp <yo1sjp@outlook.jp>
2025-04-08 11:18:45 +0900

    update step

--- js/webble.js
+++ js/webble.js
@@ -57,7 +57,11 @@ webble.init = function(opt) {
 // WEB Bluetooth へ接続
 webble.connect = function(cb_func) {
     // 同時送信数4固定
-    webhid.load_step = 4;
+    if (aztool.is_mobile()) {
+        webhid.load_step = 1;
+    } else {
+        webhid.load_step = 4;
+    }
     // デバイス情報設定
     webhid.raw_report_id = {
         "in": 1,
